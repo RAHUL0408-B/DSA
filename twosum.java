0520bd38cafee003973f7023571da2272b9515ce
RAHUL0408-B <rahulbramhankar04@gmail.com>
2026-06-23 08:44:11 +0530

    Hashmaptwosum

--- twosum.java
+++ twosum.java
@@ -1,11 +1,33 @@
-public class twosum {
+// //public class twosum {
+//     public static void main(String[] args){
+//         int[] arr = {2, 7, 11, 15};
+//         int target = 9;
+//         for (int i =0;i<arr.length;i++){
+//             for (int j=i+1;j<arr.length;j++){
+//                 if (arr[i] + arr[j] == target) {
+//                     System.out.println("Indices: " + i + ", " + j);
+//                 }            }}
+//         }
+//     }
+
+import java.util.HashMap;
+
+    public class twosum {
     public static void main(String[] args){
         int[] arr = {2, 7, 11, 15};
         int target = 9;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
         for (int i =0;i<arr.length;i++){
-            for (int j=i+1;j<arr.length;j++){
-                if (arr[i] + arr[j] == target) {
-                    System.out.println("Indices: " + i + ", " + j);
-                }            }}
+            int complement = target - arr[i];
+            if (map.containsKey(complement)) {
+                System.out.println("Indices: " + map.get(complement) + ", " + i);
+            } else {
+                map.put(arr[i], i);
+            }
         }
+
+        {
+    }
+}
     }
