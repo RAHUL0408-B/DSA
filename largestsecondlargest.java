82ce46b589851cda2b6680af1f4f9ff6289bb14a
RAHUL0408-B <rahulbramhankar04@gmail.com>
2026-06-23 07:47:57 +0530

    Secondlargets,largestelement

---
+++ largestsecondlargest.java
@@ -0,0 +1,19 @@
+public class largestsecondlargest {
+    public static void main (String[] args){
+        int arr[] = {10, 20, 4, 45, 99};
+        int largest = 0;
+        int secondLargest = 0;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] > largest){
+                secondLargest = largest;
+                largest = arr[i];
+            } else if(arr[i] > secondLargest && arr[i] != largest){
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("The largest number is: " + largest);
+        System.out.println("The second largest number is: " + secondLargest);
+    }
+}
