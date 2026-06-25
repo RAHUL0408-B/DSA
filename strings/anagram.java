88cc4a493ce388a857ebd0b1563116650e558413
RAHUL0408-B <rahulbramhankar04@gmail.com>
2026-06-25 10:41:15 +0530

    anagram

---
+++ strings/anagram.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+
+public class anagram {
+
+    public static void main(String[] args) {
+
+        String s1 = "listen";
+        String s2 = "silent";
+
+        if (s1.length() != s2.length()) {
+            System.out.println("Not Anagram");
+            return;
+        }
+
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.println("Anagram");
+        } else {
+            System.out.println("Not Anagram");
+        }
+    }
+}
