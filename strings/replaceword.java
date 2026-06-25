872bc007fa19c0a52cb24607f2de1af363095ffa
RAHUL0408-B <rahulbramhankar04@gmail.com>
2026-06-25 21:31:15 +0530

    replaceword

---
+++ strings/replaceword.java
@@ -0,0 +1,25 @@
+// public class replaceword {
+//     public static void main(String[] args) {
+//         String s = "hello my name is rahul";
+//         String replaceword = s.replace("rahul", "pawan");
+//         System.out.println(replaceword);
+
+//     }
+// }
+// public class replaceword {
+//     public static void main(String[] args) {
+//         String s = "hello my name is rahul";
+//         String replaceword = s.replace("a", "b");
+//         System.out.println(replaceword);
+
+//     }
+// }
+
+public class replaceword {
+    public static void main(String[] args) {
+        String s = "rahul is good ";
+        String replaceword = s.replace("good", "handsome");
+        System.out.println(replaceword);
+
+    }
+}
