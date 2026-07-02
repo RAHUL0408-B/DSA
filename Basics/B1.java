ce3761dfae47e6b2833aeaa2328a80de412ec72e
RAHUL0408-B <rahulbramhankar04@gmail.com>
2026-07-02 22:27:28 +0530

    10 basics questions

---
+++ Basics/B1.java
@@ -0,0 +1,9 @@
+package Basics;
+import java.util.Scanner;
+
+public class B1 {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println(n%2==0 ? "Even" : "Odd");
+
+}
